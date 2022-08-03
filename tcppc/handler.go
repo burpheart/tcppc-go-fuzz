4246fa0703d10eed39a19b8ccb7d129eb64e60a1
CZ <cfd0123456789@qq.com>
2022-08-03 12:28:55 +0800

    add  http data

--- tcppc/handler.go
+++ tcppc/handler.go
@@ -10,6 +10,63 @@ import (
 	"time"
 )
 
+const data1 = `HTTP/1.1 200 OK
+Server: nginx/1.14.2
+Content-Type: text/html
+Connection: keep-alive
+Vary: Accept-Encoding
+ETag: W/"6203b88e-264"
+X-Content-Type-Options: nosniff`
+
+const data2 = `
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to nginx!</title>
+<style>
+    body {
+        width: 35em;
+        margin: 0 auto;
+        font-family: Tahoma, Verdana, Arial, sans-serif;
+    }
+</style>
+</head>
+<body>
+<h1>Welcome to nginx!</h1>
+<p>If you see this page, the nginx web server is successfully installed and
+working. Further configuration is required.</p>
+
+<p>For online documentation and support please refer to
+<a href="http://nginx.org/">nginx.org</a>.<br/>
+Commercial support is available at
+<a href="http://nginx.com/">nginx.com</a>.</p>
+
+<p><em>Thank you for using nginx.</em></p>
+</body>
+</html>
+<!-
+root
+uid=0(root) gid=0(root) groups=0(root)
+
+root:x:0:0:root:/root:/bin/bash
+bin:x:1:1:bin:/bin:/sbin/nologin
+daemon:x:2:2:daemon:/sbin:/sbin/nologin
+adm:x:3:4:adm:/var/adm:/sbin/nologin
+lp:x:4:7:lp:/var/spool/lpd:/sbin/nologin
+sync:x:5:0:sync:/sbin:/bin/sync
+shutdown:x:6:0:shutdown:/sbin:/sbin/shutdown
+halt:x:7:0:halt:/sbin:/sbin/halt
+mail:x:8:12:mail:/var/spool/mail:/sbin/nologin
+operator:x:11:0:operator:/root:/sbin/nologin
+games:x:12:100:games:/usr/games:/sbin/nologin
+ftp:x:14:50:FTP User:/var/ftp:/sbin/nologin
+nobody:x:99:99:Nobody:/:/sbin/nologin
+success
+SUCCCESS
+MikroTik
+RouterOS
+->`
+
 func StartFuzzServer(host string, port int, config *tls.Config, writer *RotWriter, timeout int) {
 	log.Printf("Server Mode: TLS\n")
 	log.Printf("Listen: %s:%d\n", host, port)
